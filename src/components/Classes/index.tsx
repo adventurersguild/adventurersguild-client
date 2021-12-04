25b5b7dbcbc59d886a4647d1cedf7f9c92a218b8
Ethan Neidhart <ethanneid@gmail.com>
2021-12-04 17:14:14 -0500

    Prefer Formik to useFormik

--- src/components/Classes/index.tsx
+++ src/components/Classes/index.tsx
@@ -1,34 +1,35 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import { Field, Form, Formik } from 'formik';
 
 const Classes = (): React.ReactElement => {
-	const formik = useFormik({
-		initialValues: {
-			name: '',
-		},
-		onSubmit: values => {
-			alert(JSON.stringify(values, null, 2));
-		},
-	});
-
 	return (
 		<>
 			<h1>Class Builder</h1>
-			<form onSubmit={formik.handleSubmit}>
-				<label htmlFor="source">Source Name</label>
-				<select
-					id="source"
-					{...formik.getFieldProps('source')}
-				>
-				</select>
-				<label htmlFor="name">Class Name:</label>
-				<input
-					id="name"
-					type="text"
-					{...formik.getFieldProps('name')}
-				/>
-				<button type="submit">Save</button>
-			</form>
+			<Formik
+				initialValues={{
+					name: '',
+				}}
+				onSubmit={values => {
+					alert(JSON.stringify(values, null, 2));
+				}}
+			>
+				<Form>
+					<label htmlFor="source">Source Name</label>
+					<Field
+						id="source"
+						name="source"
+						as="select"
+					>
+					</Field>
+					<label htmlFor="name">Class Name:</label>
+					<Field
+						id="name"
+						name="name"
+						type="text"
+					/>
+					<button type="submit">Save</button>
+				</Form>
+			</Formik>
 		</>
 	);
 };
