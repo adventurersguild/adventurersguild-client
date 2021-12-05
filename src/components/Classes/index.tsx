e369129a5642edc694f7e1b8fc90eb2c19a083f4
Ethan Neidhart <ethanneid@gmail.com>
2021-12-04 20:07:13 -0500

    Add basic state to class builder

--- src/components/Classes/index.tsx
+++ src/components/Classes/index.tsx
@@ -1,38 +1,61 @@
 import React from 'react';
 import { Field, Form, Formik } from 'formik';
+import type { State } from './types';
 
-const Classes = (): React.ReactElement => {
-	return (
-		<>
-			<h1>Class Builder</h1>
-			<Formik
-				initialValues={{
-					name: '',
-				}}
-				onSubmit={values => {
-					alert(JSON.stringify(values, null, 2));
-				}}
-			>
-				<Form>
-					<label htmlFor="source">Source Name</label>
-					<Field
-						id="source"
-						name="source"
-						as="select"
-					>
-					</Field>
-					<label htmlFor="name">Class Name:</label>
-					<Field
-						id="name"
-						name="name"
-						type="text"
-					/>
-					<button type="submit">Save</button>
-				</Form>
-			</Formik>
-		</>
-	);
-};
+class Classes extends React.Component <unknown, State> {
+	state = {
+		sources: [],
+	};
+
+	loadSources = () => {
+		const sources = [
+			{ name: "Player's Handbook", value: 'phb' },
+			{ name: "Xanathar's Guide to Everything", value: 'xgte' },
+			{ name: "Tasha's Cauldron of Everything", value: 'tcoe' },
+		];
+		this.setState({ sources });
+	};
+
+	componentDidMount() {
+		this.loadSources();
+	}
+
+	render() {
+		return (
+			<>
+				<h1>Class Builder</h1>
+				<Formik
+					initialValues={{
+						name: '',
+					}}
+					onSubmit={values => {
+						alert(JSON.stringify(values, null, 2));
+					}}
+				>
+					<Form>
+						<label htmlFor="source">Source Name</label>
+						<Field
+							id="source"
+							name="source"
+							as="select"
+						>
+							{this.state.sources.map(({ name, value }) =>
+								<option value={value} key={value}>{name}</option>
+							)}
+						</Field>
+						<label htmlFor="name">Class Name:</label>
+						<Field
+							id="name"
+							name="name"
+							type="text"
+						/>
+						<button type="submit">Save</button>
+					</Form>
+				</Formik>
+			</>
+		);
+	}
+}
 
 export default Classes;
 
---
+++ src/components/Classes/types.tsx
@@ -0,0 +1,9 @@
+type Source = {
+	name: string,
+	value: string,
+};
+
+export type State = {
+	sources: Source[],
+};
+
